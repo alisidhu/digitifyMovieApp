cbddf57ee8d6075d6617603fe3f5ded49054f1d5
Muhammad Ali <Muhammad.Ali@macpakistan.local>
2021-06-21 14:49:43 +0500

    git navigation and prefrence added

--- app/src/main/java/com/movieapp/diditify/MainActivity.kt
+++ app/src/main/java/com/movieapp/diditify/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.movieapp.diditify
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.movieapp.diditify.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        val nav = bottomNav.setupWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/movieapp/diditify/ui/MoviesFragment.kt
+++ app/src/main/java/com/movieapp/diditify/ui/MoviesFragment.kt
@@ -7,18 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.movieapp.diditify.R
 
-class HomeFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+class MoviesFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_movies, container, false)
     }
 
 
